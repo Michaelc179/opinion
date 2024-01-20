eaaabd944391c9bfb4af567e667a3a5b21091044
Michael Chen <michaelchen179@gmail.com>
2024-01-20 13:07:05 -0500

    Initial Commit

---
+++ opinion/opinionApp.swift
@@ -0,0 +1,32 @@
+//
+//  opinionApp.swift
+//  opinion
+//
+//  Created by Michael Chen on 1/20/24.
+//
+
+import SwiftUI
+import SwiftData
+
+@main
+struct opinionApp: App {
+    var sharedModelContainer: ModelContainer = {
+        let schema = Schema([
+            Item.self,
+        ])
+        let modelConfiguration = ModelConfiguration(schema: schema, isStoredInMemoryOnly: false)
+
+        do {
+            return try ModelContainer(for: schema, configurations: [modelConfiguration])
+        } catch {
+            fatalError("Could not create ModelContainer: \(error)")
+        }
+    }()
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+        .modelContainer(sharedModelContainer)
+    }
+}
